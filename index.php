23905711e6839a08a4bf4ce13c687f4b84edab24
vidya kalappurayil viswanathan <vk427@afsconnect2.njit.edu>
2017-10-17 11:59:54 -0400

    Add index file

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+//set the error reporting On
+
+ini_set('display_errors', 'On');
+
+error_reporting(E_ALL);
+
+//Class to auto-load ther classes
+class Manage
+{
+    public static function autoload($class)
+    {
+        include 'class/'.$class . '.php';
+    }
+}
+
+spl_autoload_register(array('Manage', 'autoload'));
+
+//instantiate the program object
+$obj = new main();
+
+class main
+{
+
+    public function __construct()
+    {
+        //set to default page
+        $pageRequest = 'homepage';
+        //check for parameters
+        if (isset($_REQUEST['page'])) {
+            $pageRequest = $_REQUEST['page'];
+        }
+        //instantiate the class that is being requested
+        $page = new $pageRequest;
+
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $page->get();
+        } else {
+            $page->post();
+        }
+
+    }
+}
