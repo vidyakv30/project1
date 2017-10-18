462db1b2be0ab7d8c2ea534b8f4c2a725d9079dc
vidya kalappurayil viswanathan <vk427@afsconnect2.njit.edu>
2017-10-17 21:47:16 -0400

    Add option to load another file

--- class/errorPage.php
+++ class/errorPage.php
@@ -5,8 +5,15 @@ class errorPage extends page
 
     public function get()
     {
-        //display error message 
-        $this->html .= "Sorry, the uploaded file is not a CSV file.Please try again with a CSV file";
+        //display error message
+        $this->html .= "<h4>Error on upload. File must be CSV.<br/>Please try again.</h4>";
+
+        //display option to upload another file
+
+        $form = '<form action="index.php" method="get"
+        enctype="multipart/form-data">';
+        $form .= '<input type="submit" value="Upload Another File" name="submit">';
+        $this->html .= $form;
 
     }
 }
